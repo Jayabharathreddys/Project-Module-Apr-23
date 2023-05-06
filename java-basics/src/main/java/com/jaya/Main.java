5669516ec064a838cabaf40cb577cd2273b5c58d
Jayabharatha Reddy Sadanagiri <Jayabharathreddys@gmail.com>
2023-05-06 13:21:59 +0530

    java baics: creating a jar file
    Signed-off-by: Jayabharatha Reddy Sadanagiri <Jayabharathreddys@gmail.com>

--- java-basics/src/main/java/com/jaya/Main.java
+++ java-basics/src/main/java/com/jaya/Main.java
@@ -1,4 +1,6 @@
-package com.jaya;
+package com.jaya;//package com.jaya;
+
+import com.jaya.Greeting;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -7,6 +9,9 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+
+        Greeting g =  new Greeting();
+        System.out.println(g.greeting());
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
