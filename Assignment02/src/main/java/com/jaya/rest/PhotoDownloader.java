a3d8f5b6846b6e8e0b147c7162595e1ae9ad688d
Jayabharatha Reddy Sadanagiri <Jayabharathreddys@gmail.com>
2023-05-17 23:44:45 +0530

    Assignment 02 LIMITS completed
    Signed-off-by: Jayabharatha Reddy Sadanagiri <Jayabharathreddys@gmail.com>

--- Assignment02/src/main/java/com/jaya/rest/PhotoDownloader.java
+++ Assignment02/src/main/java/com/jaya/rest/PhotoDownloader.java
@@ -8,13 +8,22 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
+import java.util.*;
 
 public class PhotoDownloader {
     private final String SAVE_DIR = "/photos/";
 
     public void downloadPhotos(List<Photo> photos) throws IOException {
-        OkHttpClient client = new OkHttpClient();
+        System.out.println("Entered into downloadPhotos...");
+
+        int limit = 10;
+       // Set<Integer> albumIds = new HashSet<>();
+        Map<Integer, Integer> albumCounts = new HashMap<>();
+
+       /* for (Photo photo : photos) {
+            albumIds.add(photo.getAlbumId());
+            albumCounts.put(photo.getAlbumId(), albumCounts.getOrDefault(photo.getAlbumId(), 0) + 1);
+        }*/
 
         for (Photo photo : photos) {
             String url = photo.getUrl();
@@ -23,31 +32,47 @@ public class PhotoDownloader {
             String fileName = "photo_" + photoId + ".jpg";
             String savePath = SAVE_DIR + "album_" + albumId + "/" + fileName;
 
-            var request = new Request.Builder().url(url).build();
+            System.out.println("AlbumID->"+photo.getAlbumId()
+                    +" #albumCounts.get(albumId)->"+albumCounts.get(albumId));
 
-            try (Response response = client.newCall(request).execute()) {
-                if (!response.isSuccessful()) {
-                    throw new IOException("Unexpected code " + response);
-                }
-
-                InputStream inputStream = response.body().byteStream();
-                File saveDir = new File(SAVE_DIR + "album_" + albumId);
-
-                if (!saveDir.exists()) {
-                    saveDir.mkdirs();
-                }
-
-                FileOutputStream outputStream = new FileOutputStream(savePath);
-                byte[] buffer = new byte[1024];
-                int bytesRead;
-
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    outputStream.write(buffer, 0, bytesRead);
-                }
-
-                outputStream.close();
-                inputStream.close();
+            if (albumCounts.getOrDefault(photo.getAlbumId(), 0) <= limit) {
+                startDownload(url,albumId,savePath);
+               // albumCounts.put(albumId, albumCounts.get(albumId) + 1);
+                albumCounts.put(photo.getAlbumId(), albumCounts.getOrDefault(photo.getAlbumId(), 0) + 1);
             }
+
+           /* if (albumCounts.get(albumId) > limit) {
+                break;
+            }*/
+        }
+    }
+
+    private void startDownload(String url, int albumId, String savePath) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        var request = new Request.Builder().url(url).build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response);
+            }
+
+            InputStream inputStream = response.body().byteStream();
+            File saveDir = new File(SAVE_DIR + "album_" + albumId);
+
+            if (!saveDir.exists()) {
+                saveDir.mkdirs();
+            }
+
+            FileOutputStream outputStream = new FileOutputStream(savePath);
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+
+            outputStream.close();
+            inputStream.close();
         }
     }
 }
--- Assignment02/src/main/java/com/jaya/rest/TestDownload.java
+++ Assignment02/src/main/java/com/jaya/rest/TestDownload.java
@@ -3,13 +3,13 @@ package com.jaya.rest;
 import java.io.IOException;
 import java.util.List;
 
-public class PhotoController {
+public class TestDownload {
     public static void main(String[] args) throws IOException {
 
         var restClient = new RestClient();
         List<Photo> photos = restClient.getApi().getPhotos().execute().body();
         var apiResponse = restClient.getApi().getPhotos().execute();
-        apiResponse.body().forEach(photo -> System.out.println("Titles->"+photo.getTitle()));
+        apiResponse.body().forEach(photo -> System.out.println("AlbumID->"+photo.getAlbumId()+" #Titles->"+photo.getTitle()));
         var photoDownloader = new PhotoDownloader();
         photoDownloader.downloadPhotos(photos);
     }
